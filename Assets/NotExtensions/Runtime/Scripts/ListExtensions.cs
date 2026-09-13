babd3bfb2fd4896643ff98a4af3a02099cc40dcc
NotBura <not.bura.golden.eggs@gmail.com>
2026-09-13 16:47:41 +0900

    feat: ListPrivateFieldAccessをShaderの実装に存在するListの内部配列取り出し方法を実装

--- Assets/NotExtensions/Runtime/Scripts/ListExtensions.cs
+++ Assets/NotExtensions/Runtime/Scripts/ListExtensions.cs
@@ -1,157 +1,38 @@
 using System;
 using System.Collections.Generic;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace NotBura.Packages
 {
     public static class ListExtensions
     {
-        /// <summary>
-        /// Generic type cachingパターンを用いるキャッシュクラス
-        /// </summary>
-        private class Cache<T>
+        private sealed class ListPrivateFieldAccess<T>
         {
-            // NOTE: getterの生成は重たいのでstatic field constructorで安全にキャッシュ
-            private static readonly Func<List<T>, T[]> s_getter = CreateGetter();
-
-            private static readonly FieldInfo s_fieldInfo = CreateFieldInfo();
-
-            public static Func<List<T>, T[]> Getter
-            {
-                [MethodImpl(MethodImplOptions.AggressiveInlining)]
-                get => s_getter;
-            }
-
-            public static FieldInfo FieldInfo
-            {
-                [MethodImpl(MethodImplOptions.AggressiveInlining)]
-                get => s_fieldInfo;
-            }
-
-            private static Func<List<T>, T[]> CreateGetter()
-            {
-                var _fieldInfo = CreateFieldInfo();
-
-                return _fieldInfo.ToGetter<List<T>, T[]>();
-            }
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            private static FieldInfo CreateFieldInfo()
-            {
-                var _fieldInfo = typeof(List<T>)
-                    .GetField(s_name, BindingFlags.NonPublic | BindingFlags.Instance);
-
-                return _fieldInfo;
-            }
+            internal T[] _items;
+            internal int _size;
+            internal int _version;
         }
 
-        // NOTE: 毎度GetField"s"しない為の名前のキャッシュ
-        // static string確保してしまうがしょうがない
-        private static readonly string s_name = GetFieldName();
-
-        #region AsSpanTemporary Method
-
-        // NOTE: Cache<T>を通さないので呼ぶ回数が極端に少ないならメモリと速度の観点で得になる(はず)
-        public static Span<T> AsSpanTemporary<T>(this List<T> source)
-        {
-            var _array = GetArrayTemporary(source);
-            return _array.AsSpan(0, source.Count);
-        }
-
-        public static Span<T> AsSpanTemporary<T>(this List<T> source, int start)
-        {
-            var _array = GetArrayTemporary(source);
-            return _array.AsSpan(start, source.Count - start);
-        }
-
-        public static Span<T> AsSpanTemporary<T>(this List<T> source, int start, int length)
-        {
-            var _array = GetArrayTemporary(source);
-            return _array.AsSpan(start, length);
-        }
-
-        public static Span<T> AsSpanTemporary<T>(this List<T> source, Index index)
-        {
-            var _array = GetArrayTemporary(source);
-            return _array.AsSpan(index);
-        }
-
-        public static Span<T> AsSpanTemporary<T>(this List<T> source, Range range)
-        {
-            var _array = GetArrayTemporary(source);
-            return _array.AsSpan(range);
-        }
-
-        #endregion AsSpanTemporary Method
-
-        #region AsSpan Method
+        #region as span
 
         public static Span<T> AsSpan<T>(this List<T> source)
         {
-            var _array = GetArray(source);
-            return _array.AsSpan(0, source.Count);
+            var _cast = Unsafe.As<ListPrivateFieldAccess<T>>(source);
+            return _cast._items.AsSpan(0, _cast._size);
         }
 
         public static Span<T> AsSpan<T>(this List<T> source, int start)
         {
-            var _array = GetArray(source);
-            return _array.AsSpan(start, source.Count - start);
+            var _cast = Unsafe.As<ListPrivateFieldAccess<T>>(source);
+            return _cast._items.AsSpan(start, _cast._size - start);
         }
 
         public static Span<T> AsSpan<T>(this List<T> source, int start, int length)
         {
-            var _array = GetArray(source);
-            return _array.AsSpan(start, length);
+            var _cast = Unsafe.As<ListPrivateFieldAccess<T>>(source);
+            return _cast._items.AsSpan(0, _cast._size);
         }
 
-        public static Span<T> AsSpan<T>(this List<T> source, Index index)
-        {
-            var _array = GetArray(source);
-            return _array.AsSpan(index);
-        }
-
-        public static Span<T> AsSpan<T>(this List<T> source, Range range)
-        {
-            var _array = GetArray(source);
-            return _array.AsSpan(range);
-        }
-
-        #endregion AsSpan Method
-
-        private static string GetFieldName()
-        {
-            var _fields = typeof(List<>).GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
-            foreach (var field in _fields)
-            {
-                if (field.FieldType.IsArray)
-                {
-                    return field.Name;
-                }
-            }
-
-            return null;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static T[] GetArrayTemporary<T>(List<T> source)
-        {
-            var _fieldInfo = typeof(List<T>)
-                .GetField(s_name, BindingFlags.NonPublic | BindingFlags.Instance);
-            var _result = (T[])_fieldInfo.GetValue(source);
-            return _result;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static T[] GetArray<T>(List<T> source)
-        {
-            var _array =
-#if ENABLE_LIST_EXTENSIONS_REFLECTION
-                (T[])Cache<T>.FieldInfo.GetValue(source);
-#else
-                Cache<T>.Getter(source);
-#endif
-            return _array;
-        }
+        #endregion as span
     }
 }
